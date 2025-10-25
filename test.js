6d4fcac3b4cfd54246177529de54021c1be40979
alexstmsal <alex.stmsal@gmail.com>
2025-10-25 22:50:08 +0200

    Primer cambio de prueba

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
+let fecha = new Date();
+let h = fecha.getHours();
+let m = fecha.getMinutes();
+let s = fecha.getSeconds();
 
-  let fecha = new Date();
-  let h = fecha.getHours();
-  let m = fecha.getMinutes();
-  let s = fecha.getSeconds();
+let hora = `${h}:${m}:${s}`;
 
-  let hora = `${h}:${m}:${s}`;
-  
-  console.log(hora);
+console.log(hora);
+console.log("Cambios!");
